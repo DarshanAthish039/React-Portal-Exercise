f36af3bb33c0b9673f929158261f2c2080a37433
DarshanAthish039 <darshanathish934@gmail.com>
2025-09-18 20:14:34 +0530

    Update Login.js

--- FOOD DELIVERY/Login.js
+++ FOOD DELIVERY/Login.js
@@ -1,54 +1,64 @@
-import React, { useState } from 'react';
-import './Login.css';
-import { Link, useNavigate } from 'react-router-dom'; // Import useNavigate
+package runner;
 
-const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const navigate = useNavigate(); // Initialize navigate
+import org.openqa.selenium.By;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
-  const handleLogin = (e) => {
-    e.preventDefault();
-    // Perform login logic here
-    console.log('Logging in with:', { email, password });
-    
-    // After successful login, redirect to Home page
-    navigate('/home');
-  };
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
 
-  return (
-    <div className="login-container">
-      <div className="login-box">
-        <h2>Login to Food Delivery</h2>
-        <form onSubmit={handleLogin}>
-          <div className="input-group">
-            <label htmlFor="email">Email:</label>
-            <input
-              type="email"
-              id="email"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <div className="input-group">
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="login-button">Login</button>
-        </form>
-        <div className="signup-link">
-          <p>Don't have an account? <Link to="/signup">Sign up here</Link></p>
-        </div>
-      </div>
-    </div>
-  );
-};
+import utils.Base;
+import utils.ExcelReader;
+import utils.Reporter;
+import utils.Screenshot;
+import utils.WebDriverHelper;
 
-export default Login;
+public class TestRunner extends Base {
+
+    public WebDriverHelper helper;
+    public String path="resources/Testdata.xlsx";
+    public String name="Sheet1";
+    public Screenshot screenshot;
+    public ExtentTest test;
+    public ExtentReports extentReports;
+
+    @BeforeMethod
+    public void launchBrowser() {
+        openBrowser();
+        helper=new WebDriverHelper(driver);
+        extentReports=Reporter.generateExtentReport("MyTestReport");
+    }
+
+    @Test
+    public void testOne() throws Exception {
+        test=extentReports.createTest("New-test");
+        String email=ExcelReader.readdata(path, name, 0, 0);
+        String pass=ExcelReader.readdata(path,name,1,0);
+
+        helper.clickOnElement(By.xpath("//input[@type='email']"));
+
+        helper.sendKeys(By.xpath("//input[@type='email']"), email);
+
+        helper.clickOnElement(By.xpath("//input[@type='password']"));
+
+        helper.sendKeys(By.xpath("//input[@type='password']"), pass);
+
+        helper.clickOnElement(By.xpath("//label[text()='Remember me']"));
+
+        helper.clickOnElement(By.xpath("//input[@type='submit']"));
+
+        helper.clickOnElement(By.xpath("//a[@id='forgot_password_link']"));
+
+        Screenshot.getScreenShot(driver, "image");
+
+        Reporter.attachScreenshotToReport(test,driver,"image1");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+        extentReports.flush();
+    }
+}
