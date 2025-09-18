d418732086c9afeea51cc588fbed1c1a01408b7c
DarshanAthish039 <darshanathish934@gmail.com>
2025-09-18 12:55:39 +0530

    Update App.js

--- FOOD DELIVERY/App.js
+++ FOOD DELIVERY/App.js
@@ -265,3 +265,15 @@ log report found
 log4j.appender.RollingAppender.File=logs/default.log 
 
 
+# Define the root logger with the DailyRollingFileAppender
+log4j.rootLogger=DEBUG, RollingAppender
+
+# Configure the DailyRollingFileAppender
+log4j.appender.RollingAppender=org.apache.log4j.DailyRollingFileAppender
+log4j.appender.RollingAppender.File=logs/default.log
+log4j.appender.RollingAppender.DatePattern='.'yyyy-MM-dd_HH-mm-ss
+log4j.appender.RollingAppender.layout=org.apache.log4j.PatternLayout
+log4j.appender.RollingAppender.layout.ConversionPattern=[%d{yyyy-MM-dd HH:mm:ss}] [%p] %c %M - %m%n
+
+
+
